2fbf383a0d88ce80a7423b5f92c77ad43d017c4d
Shota Osawa <syota.osawa@gmail.com>
2018-11-10 15:31:04 +0900

    xx

--- app.js
+++ app.js
@@ -60,17 +60,17 @@ function getMessageText(text) {
   } else {
     message = '?????????????';
   }
-//  return message;
+  return message;
 
- var price = 0;
- if(text.match(/カット/)){
-   price += 1000;
- }
- if(text.match(/パーマ/)){
-   price += 2000;
- }
- message = "価格は" + price + "円です。";
- return message;
+  var price = 0;
+  if(text.match(/カット/)){
+    price += 1000;
+  }
+  if(text.match(/パーマ/)){
+    price += 2000;
+  }
+  message = "価格は" + price + "円です。";
+  return message;
 
 
 // function getmassageText(text){
